2084e5ec058d9683335d29c418022af7e231d2f9
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2023-06-03 02:10:25 +0900

    add create for runtime integration tests

---
+++ crates/lelele-runtime-tests/build.rs
@@ -0,0 +1,32 @@
+use anyhow::Context;
+use lelele::{
+    codegen::ParserDefinition,
+    grammar::{Grammar, GrammarDef},
+};
+use lelele_tests::grammars;
+use std::{env, fs, path::PathBuf};
+
+fn main() -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    generate_parser("g_simple1", grammars::g_simple1).context("generating g_simple1")?;
+
+    Ok(())
+}
+
+fn generate_parser(
+    name: &str,
+    grammar_def: impl FnOnce(&mut GrammarDef<'_>),
+) -> anyhow::Result<()> {
+    let grammar = Grammar::define(grammar_def);
+    let parser_def = ParserDefinition::new(&grammar);
+
+    let out_dir = env::var("OUT_DIR").map(PathBuf::from).unwrap().join(name);
+    fs::create_dir_all(&out_dir)
+        .with_context(|| format!("create_dir_all({})", out_dir.display()))?;
+    let out_path = out_dir.join("parser.rs");
+    fs::write(&out_path, parser_def.to_string())
+        .with_context(|| format!("write({}, <parser definition>)", out_path.display()))?;
+
+    Ok(())
+}
---
+++ crates/lelele-runtime-tests/src/g_simple1.rs
@@ -0,0 +1,73 @@
+pub mod p {
+    include!(concat!(env!("OUT_DIR"), "/g_simple1/parser.rs"));
+}
+#[allow(unused_imports)]
+use lelele_runtime::parser::{ParseEvent::*, ParseItem::*};
+use std::convert::Infallible;
+
+#[derive(Debug)]
+pub struct Token(p::SymbolID);
+impl lelele_runtime::parser::Token<p::SymbolID> for Token {
+    fn as_symbol(&self) -> p::SymbolID {
+        self.0
+    }
+}
+
+pub fn tokens<I>(iter: I) -> impl Iterator<Item = Result<Token, Infallible>>
+where
+    I: IntoIterator<Item = p::SymbolID>,
+{
+    iter.into_iter().map(|t| Ok(Token(t)))
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_matches {
+    ($($t:tt)*) => { assert!(matches!($($t)*)) };
+}
+
+#[test]
+fn case1() {
+    let mut tokens = tokens([p::SymbolID::NUM, p::SymbolID::EQUAL, p::SymbolID::NUM]);
+    let mut parser = p::parser::<Token>();
+    let mut args = vec![];
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!(
+        (event, &args[..]),
+        (Reduce(p::RuleID::T_0), [T(Token(p::SymbolID::NUM))])
+    );
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!(
+        (event, &args[..]),
+        (Reduce(p::RuleID::E_1), [N(p::SymbolID::T)])
+    );
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!(
+        (event, &args[..]),
+        (Reduce(p::RuleID::T_0), [T(Token(p::SymbolID::NUM))])
+    );
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!(
+        (event, &args[..]),
+        (Reduce(p::RuleID::E_1), [N(p::SymbolID::T)])
+    );
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!(
+        (event, &args[..]),
+        (
+            Reduce(p::RuleID::A_0),
+            [
+                N(p::SymbolID::E),
+                T(Token(p::SymbolID::EQUAL)),
+                N(p::SymbolID::E)
+            ]
+        )
+    );
+
+    let event = parser.next_event(&mut tokens, &mut args).unwrap();
+    assert_matches!((event, &args[..]), (Accept, [N(p::SymbolID::A)]));
+}
