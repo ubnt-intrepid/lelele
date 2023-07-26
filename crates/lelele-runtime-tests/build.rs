1be6458528fe808d90402c6d31fa947dd900c5e0
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2023-07-26 15:57:08 +0900

    re-enable runtime-tests build script

--- crates/lelele-runtime-tests/build.rs
+++ crates/lelele-runtime-tests/build.rs
@@ -1,11 +1,9 @@
-// use std::{env, path::PathBuf};
+use std::{env, path::PathBuf};
 
 fn main() {
-    /*
     let project_root = env::var_os("CARGO_MANIFEST_DIR")
         .map(PathBuf::from)
         .unwrap();
     let test_grammars_dir = project_root.join("../lelele/tests").canonicalize().unwrap();
     lelele::build::process_dir(&test_grammars_dir).unwrap();
-    */
 }
